8f48b56862d3db9aa3a588d08582916a35e2ca15
Bakyt <isimovabakyt@gmail.com>
2019-10-06 12:00:52 +0600

    postgres

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'travel.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travel',
+            'USER': 'travel',
+            'PASSWORD': 'travel',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
     }
-}
 
 
 # Password validation
