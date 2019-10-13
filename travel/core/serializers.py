e1b238c38c9d1afde8d197226f386f9716c66cf9
dynomite <gigit@github.com>
2019-10-13 12:35:01 +0600

    ADD: search function by name

--- travel/core/serializers.py
+++ travel/core/serializers.py
@@ -5,5 +5,18 @@ from .models import Place
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
-        fields = ('id', 'name', 'address')
+        fields = '__all__'
+        depth = 2
 
+
+class PlaceSearchSerializer(serializers.Serializer):
+
+    def search(self, query):
+        from django.contrib.postgres.search import TrigramSimilarity
+        from utils import constants
+        kwargs = {}
+        kwargs['similarity__gt'] = constants.SEARCH_SIMILARITY
+        places = []
+        for place in Place.objects.annotate(similarity=TrigramSimilarity('name', query), ).filter(**kwargs):
+            places.append(place)
+        return {'places': PlaceSerializer(places, many=True).data}
--- travel/core/views.py
+++ travel/core/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from .serializers import PlaceSerializer
+from .serializers import PlaceSerializer, PlaceSearchSerializer
 from .models import Place, Country, City, PlaceType, PlaceService
 from utils.decorators import response_wrapper
 
@@ -13,6 +13,11 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Place.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = PlaceSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'search':
+            return PlaceSearchSerializer
+        return self.serializer_class
 
     def get_queryset(self):
         queryset = self.queryset
@@ -29,3 +34,15 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
         if price_max:
             queryset = queryset.filter(price__lte=price_max)
         return queryset
+
+    @action(methods=['post'], detail=False)
+    def search(self, request):
+        query = request.data['query']
+        print(1)
+        serializer = self.get_serializer(data=request.data)
+        print(2)
+        serializer.is_valid(raise_exception=True)
+        print(3)
+        result = serializer.search(query=query)
+        print("result:", result)
+        return Response(result)
