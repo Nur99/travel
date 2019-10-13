bad025fa31f20adf16f03414f5d359e898fdefb0
dynomite <gigit@github.com>
2019-10-13 11:40:56 +0600

    CHANGE: filter finished

--- travel/core/views.py
+++ travel/core/views.py
@@ -15,8 +15,17 @@ class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PlaceSerializer
 
     def get_queryset(self):
-        queryset = Place.objects.all()
-        if self.request.query_params.get("city"):
-            queryset = queryset.filter(city__name='Almaty')
+        queryset = self.queryset
+        city = self.request.query_params.get('city')
+        if city:
+            queryset = queryset.filter(city=City.objects.get(id=city))
+        country = self.request.query_params.get('country')
+        if country:
+            queryset = queryset.filter(city__country=Country.objects.get(id=country))
+        price_min = self.request.query_params.get('price_min')
+        price_max = self.request.query_params.get('price_max')
+        if price_min:
+            queryset = queryset.filter(price__gte=price_min)
+        if price_max:
+            queryset = queryset.filter(price__lte=price_max)
         return queryset
-
