6a55e8b464c61eb23c5fc229a9c7dee42134cac5
Bakyt <isimovabakyt@gmail.com>
2019-11-03 14:51:49 +0600

    country, city viewset

--- travel/core/admin.py
+++ travel/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Country, City, Place, PlaceType, PlaceService, PlaceUserRating
+from .models import (Country, City, Place, PlaceType, PlaceService,
+                    PlaceUserRating)
 
 
 @admin.register(City)
--- travel/core/urls.py
+++ travel/core/urls.py
@@ -1,8 +1,10 @@
-from .views import PlaceViewSet
+from .views import PlaceViewSet, CityViewSet, CountryViewSet
 from rest_framework.routers import DefaultRouter
 
 app_name = 'travel'
 
 router = DefaultRouter()
 router.register(r'place', PlaceViewSet, base_name='place')
+router.register(r'city', CityViewSet, base_name='city')
+router.register(r'country', CountryViewSet, base_name='country')
 urlpatterns = router.urls
